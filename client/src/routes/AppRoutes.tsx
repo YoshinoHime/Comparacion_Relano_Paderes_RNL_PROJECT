dfe28eb441118fb676bd7236255a263ec9c04094
PZDJX <ysraelrelano@gmail.com>
2026-04-17 21:38:22 +0800

    Part 7n8

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,53 +1,22 @@
-import { Route, Routes } from "react-router-dom"
-import AppLayout from "../Layout/AppLayout"
-import FloatingLabelInput from "../components/input/FloatingLabelInput";
-import { useState } from "react";
-
-const SampleComponent = () => {
-    const [firstname, setFirstName] = useState (" ");
-    const [lastname, setLastName] = useState (" ");
-    const [birthdate, setBirthDate] = useState (" ");
-    const [password, setPassword] = useState (" ");
-
-    return (
-        <>
-          <h1 className="text-red-600">Hello World</h1>
-          <div className="mb-4">
-          <FloatingLabelInput label="First Name"
-          type="text" name="first_name" value={firstname} onChange={(e) => setFirstName(e.target.value)} required autoFocus/>
-          <p className="font-medium">First Name: {firstname}</p>
-
-          </div>
-          <div className="mb-4">
-          <FloatingLabelInput label="Last Name" type="text" name="last_name" value={lastname} onChange={(e) => setLastName(e.target.value)} required/>
-          <p className="font-medium">Last Name: {lastname}</p>
-          </div>
-
-          <div className="mb-4">
-          <FloatingLabelInput label="Birth Date"
-          type="date" name="birth_date" value={birthdate} onChange={(e) => setBirthDate(e.target.value)} />
-          <p className="font-medium">Birth Date: {birthdate}</p>
-          </div>
-
-          <div className="mb-4">
-          <FloatingLabelInput label="Password" 
-          type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
-          <p className="font-medium">Password: {password}</p>
-          </div>
-        </>
-    );
-};
+import { Route, Routes } from "react-router-dom";
+import AppLayout from "../layout/AppLayout";
+import GenderMainPage from "../pages/Gender/Gender/GenderMainPage";
+import EditGenderPage from "../pages/Gender/EditGenderPage";
+import DeleteGenderPage from "../pages/Gender/DeleteGenderPage";
+import UserMainPage from "../pages/User/UserMainPage";
 
 const AppRoutes = () => {
   return (
-    <>
     <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<SampleComponent/> }/>
-        </Route> 
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<GenderMainPage />} />
+        <Route path="/gender/edit" element={<EditGenderPage />} />
+        <Route path="/gender/delete" element={<DeleteGenderPage />} />
+        <Route path="/users" element={<UserMainPage />} />
+      </Route>
     </Routes>
-    </>
   );
 };
 
-export default AppRoutes
+export default AppRoutes;
+
